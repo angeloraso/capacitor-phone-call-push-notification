e66e8ab9d8e5435b53f2d0568acd6b1110904a54
Angelo Raso <angeloraso6@gmail.com>
2025-02-22 11:24:11 -0300

    Fix activity content

--- android/src/main/java/ar/com/anura/plugins/phonecallpushnotification/PhoneCallPushNotification.java
+++ android/src/main/java/ar/com/anura/plugins/phonecallpushnotification/PhoneCallPushNotification.java
@@ -1,68 +1,54 @@
 package ar.com.anura.plugins.phonecallpushnotification;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.util.Log;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 import java.util.Objects;
+import java.util.function.Consumer;
 
-public class PhoneCallPushNotificationActivity extends AppCompatActivity {
-    private static final String TAG = "PhoneCallPushNotificationActivity";
-    static final String PREFERENCES_KEY = "PhoneCallPushNotificationActivityKey";
+public class PhoneCallPushNotification {
+    private static final String TAG = "PhoneCallPushNotification";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public static void registerPushNotifications(Context context, PhoneCallPushNotificationSettings settings, Consumer<String> onPushNotificationTokenEvent) {
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        FirebaseMessaging
+                .getInstance()
+                .getToken()
+                .addOnCompleteListener(
+                        task -> {
+                            if (!task.isSuccessful()) {
+                                try {
+                                    throw new Exception(Objects.requireNonNull(task.getException()).getLocalizedMessage());
+                                } catch (Exception e) {
+                                    Log.d(TAG, "registerPushNotifications error: " + e.getMessage());
+                                    throw new RuntimeException(e);
+                                }
+                            }
 
-        setShowWhenLocked(true);
-        setTurnScreenOn(true);
-
-        Intent intent = getIntent();
-        Context context = getApplicationContext();
-
-        Log.d(TAG, "Phone call push notification action has been received in activity");
-
-        int notificationId = intent.getIntExtra("notificationId", -1);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.cancel(notificationId);
-
-        String response = intent.getAction();
-        String origin = intent.getStringExtra("origin");
-        long timestamp = intent.getLongExtra("timestamp", System.currentTimeMillis());
-
-        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("origin", origin);
-        editor.putLong("timestamp", timestamp);
-        editor.putString("response", response);
-        editor.apply();
-
-        if (Objects.equals(response, "tap") || Objects.equals(response, "answer")) {
-            Class<? extends AppCompatActivity> mainActivity = getMainActivityClass(context);
-            Intent mainIntent = new Intent(context, mainActivity);
-            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            context.startActivity(mainIntent);
-        }
-
-        finish();
+                            Log.d(TAG, "FCM remote message: " + task.getResult());
+                            PhoneCallPushNotificationSettings.saveSettings(context, settings);
+                            onPushNotificationTokenEvent.accept(task.getResult());
+                        }
+                );
     }
 
-    private Class<? extends AppCompatActivity> getMainActivityClass(Context context) {
+    public static boolean areNotificationsEnabled(Context context) {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context.getApplicationContext());
+        return notificationManager.areNotificationsEnabled();
+    }
+
+    public static void unregisterPushNotifications(Context context) {
         try {
-            String packageName = context.getPackageName();
-            Class<?> mainActivityClass = Class.forName(packageName + ".MainActivity");
-            if (AppCompatActivity.class.isAssignableFrom(mainActivityClass)) {
-                return mainActivityClass.asSubclass(AppCompatActivity.class);
-            } else {
-                throw new RuntimeException("MainActivity does not extend AppCompatActivity.");
-            }
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Unable to resolve MainActivity class.");
+            PhoneCallPushNotificationSettings.clear(context);
+            FirebaseMessaging.getInstance().setAutoInitEnabled(false);
+            FirebaseMessaging.getInstance().deleteToken();
+        } catch (Exception e) {
+            Log.d(TAG, "unregisterPushNotifications error: " + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 }
--- android/src/main/java/ar/com/anura/plugins/phonecallpushnotification/PhoneCallPushNotificationActivity.java
+++ android/src/main/java/ar/com/anura/plugins/phonecallpushnotification/PhoneCallPushNotificationActivity.java
@@ -12,57 +12,57 @@ import androidx.core.app.NotificationManagerCompat;
 import java.util.Objects;
 
 public class PhoneCallPushNotificationActivity extends AppCompatActivity {
-  private static final String TAG = "PhoneCallPushNotificationActivity";
-  static final String PREFERENCES_KEY = "PhoneCallPushNotificationActivityKey";
+    private static final String TAG = "PhoneCallPushNotificationActivity";
+    static final String PREFERENCES_KEY = "PhoneCallPushNotificationActivityKey";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    setShowWhenLocked(true);
-    setTurnScreenOn(true);
+        setShowWhenLocked(true);
+        setTurnScreenOn(true);
 
-    Intent intent = getIntent();
-    Context context = getApplicationContext();
+        Intent intent = getIntent();
+        Context context = getApplicationContext();
 
-    Log.d(TAG, "Phone call push notification action has been received in activity");
+        Log.d(TAG, "Phone call push notification action has been received in activity");
 
-    int notificationId = intent.getIntExtra("notificationId", -1);
-    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-    notificationManager.cancel(notificationId);
+        int notificationId = intent.getIntExtra("notificationId", -1);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.cancel(notificationId);
 
-    String response = intent.getAction();
-    String origin = intent.getStringExtra("origin");
-    String expiration = intent.getStringExtra("expiration");
+        String response = intent.getAction();
+        String origin = intent.getStringExtra("origin");
+        long timestamp = intent.getLongExtra("timestamp", System.currentTimeMillis());
 
-    SharedPreferences sharedPreferences = context.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
-    SharedPreferences.Editor editor = sharedPreferences.edit();
-    editor.putString("origin", origin);
-    editor.putString("expiration", expiration);
-    editor.putString("response", response);
-    editor.apply();
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("origin", origin);
+        editor.putLong("timestamp", timestamp);
+        editor.putString("response", response);
+        editor.apply();
 
-    if (Objects.equals(response, "tap") || Objects.equals(response, "answer")) {
-      Class<? extends AppCompatActivity> mainActivity = getMainActivityClass(context);
-      Intent mainIntent = new Intent(context, mainActivity);
-      mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-      context.startActivity(mainIntent);
+        if (Objects.equals(response, "tap") || Objects.equals(response, "answer")) {
+            Class<? extends AppCompatActivity> mainActivity = getMainActivityClass(context);
+            Intent mainIntent = new Intent(context, mainActivity);
+            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            context.startActivity(mainIntent);
+        }
+
+        finish();
     }
 
-    finish();
-  }
-
-  private Class<? extends AppCompatActivity> getMainActivityClass(Context context) {
-    try {
-      String packageName = context.getPackageName();
-      Class<?> mainActivityClass = Class.forName(packageName + ".MainActivity");
-      if (AppCompatActivity.class.isAssignableFrom(mainActivityClass)) {
-        return mainActivityClass.asSubclass(AppCompatActivity.class);
-      } else {
-        throw new RuntimeException("MainActivity does not extend AppCompatActivity.");
-      }
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException("Unable to resolve MainActivity class.");
+    private Class<? extends AppCompatActivity> getMainActivityClass(Context context) {
+        try {
+            String packageName = context.getPackageName();
+            Class<?> mainActivityClass = Class.forName(packageName + ".MainActivity");
+            if (AppCompatActivity.class.isAssignableFrom(mainActivityClass)) {
+                return mainActivityClass.asSubclass(AppCompatActivity.class);
+            } else {
+                throw new RuntimeException("MainActivity does not extend AppCompatActivity.");
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Unable to resolve MainActivity class.");
+        }
     }
-  }
 }
